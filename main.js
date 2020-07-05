61fb366fd90981c22fba6a3c6cf36445d65739f0
Emre Sahin <doguruzgar@gmail.com>
2020-07-05 13:24:55 +0300

    unnecesarry

--- main.js
+++ main.js
@@ -22,25 +22,25 @@ log.info('App starting...');
 //
 // THIS SECTION IS NOT REQUIRED
 //-------------------------------------------------------------------
-let template = []
-if (process.platform === 'darwin') {
-  // OS X
-  const name = app.getName();
-  template.unshift({
-    label: name,
-    submenu: [
-      {
-        label: 'About ' + name,
-        role: 'about'
-      },
-      {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click() { app.quit(); }
-      },
-    ]
-  })
-}
+// let template = []
+// if (process.platform === 'darwin') {
+//   // OS X
+//   const name = app.getName();
+//   template.unshift({
+//     label: name,
+//     submenu: [
+//       {
+//         label: 'About ' + name,
+//         role: 'about'
+//       },
+//       {
+//         label: 'Quit',
+//         accelerator: 'Command+Q',
+//         click() { app.quit(); }
+//       },
+//     ]
+//   })
+// }
 
 
 //-------------------------------------------------------------------
